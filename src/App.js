ac7323cfad7b4557e67fc4bbd07f7e3f4aa1c3d2
Josue Narro <josue.narro.a@gmail.com>
2021-03-26 05:59:21 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -3,35 +3,21 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
+        <Header />
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Text>Edit and save to reload.</Text>
           </VStack>
         </Grid>
       </Box>
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Flex, Heading, Spacer } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+
+const Header = () => {
+  return (
+    <Flex as="header" p={5}>
+      <Spacer />
+      <Heading as="h1" size="xl" my={5} marginLeft={10}>
+        Movie Rental
+      </Heading>
+      <Spacer />
+      <ColorModeSwitcher justifySelf="flex-end" />
+    </Flex>
+  );
+};
+
+export default Header;
